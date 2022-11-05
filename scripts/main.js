03e9e5c3c5799682a6458d090a77c5143bbb70ef
micaleiva01 <113473523+micaleiva01@users.noreply.github.com>
2022-11-04 23:43:09 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -20,7 +20,6 @@ door.onclick = () => {
 
 
 
-// designate elements to be used
 const divp = document.querySelector('.kitten');
 const image = document.querySelector('.rotate-right');
 
@@ -28,46 +27,14 @@ const image = document.querySelector('.rotate-right');
 const text1 = "The Century Tower's plaque.";
 const text2 = "The Century Tower's plaque.";
 
-// NOTE that two CLASSES are defined in the CSS:
-// .rotate-right
-// .rotate-left
-// these make the code below work
 
 image.onmouseenter = (e) => {
-	// change image tilt by changing a CSS class
    e.target.classList.remove('rotate-right');
    e.target.classList.add('rotate-left');
-   // change text
    divp.textContent = text2;
 };
 image.onmouseleave = (e) => {
-// change image tilt back by changing a CSS class
    e.target.classList.add('rotate-right');
    e.target.classList.remove('rotate-left');
-   // change text back
    divp.textContent = text1;
 };
-
-
-
-
-
-
-/*
-const clicker = document.getElementByID('button');
-const plaque = document.querySelector('#image');
-
-plaque.style.display = 'none';
-
-clicker.onclick = showAndHide;
-
-function showAndHide(){
-  if (plaque.classList.contains('showing')){
-    plaque.style.display = 'none';
-    plaque.classList.remove('showing');
-  } else {
-    plaque.style.display = 'block';
-    plaque.classList.add('showing');
-  }
-}
-*/
